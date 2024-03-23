39602afc24b0ba8c0700bf3bb4e5554436b92caf
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-03-23 23:29:01 +0300

    Commit

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace Geometry
+{
+    public class Vector
+    {
+        public double X;
+        public double Y;
+    }
+
+    public class Geometry
+    {
+        public static double GetLength(Vector vector)
+        {
+            return Math.Sqrt((vector.X * vector.X) + (vector.Y * vector.Y));
+        }
+
+        public static Vector Add(Vector vector1, Vector vector2)
+        {
+            Vector sum = new() { X = vector1.X + vector2.X, Y = vector1.Y + vector2.Y};
+            return sum;
+        }
+    }
+}
